bb863aaf417c6a53648c32ab0356e7411c21c71b
yusufmahendra <yusuf260901@gmail.com>
2023-02-18 19:30:48 +0700

    taks 2

---
+++ minggu7/array.php
@@ -0,0 +1,20 @@
+<?php
+//cara lama
+$hari = array("senin", "selasa", "rabu");
+//memanggil array
+//var_dump($hari);
+//echo $hari[0];
+// print_r($hari);
+// echo "<br>";
+
+// //cara baru
+// $bulan = ["januari", "february", "maret"];
+// print_r($bulan);
+
+print_r($hari);
+$hari[] = "kamis";
+echo "<br>";
+print_r($hari);
+
+?>
+
